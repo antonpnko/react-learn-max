82bff6e03081fc7c562f88cd6829f1d76ab34afe
Anton P <anton.pnko@gmail.com>
2023-03-22 12:13:51 +0200

    Relocate

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,7 +1,7 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
-import './Card.css'
-import Card from "./Card";
+import '../UI/Card.css'
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
   return (
